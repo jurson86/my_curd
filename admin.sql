76adcdead14bd44f459590e8d71681371d830dd7
zhangchuang <15238002477>
2017-11-19 18:27:06 +0800

    new

---
+++ admin.sql
@@ -0,0 +1,188 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50719
+Source Host           : localhost:3306
+Source Database       : admin
+
+Target Server Type    : MYSQL
+Target Server Version : 50719
+File Encoding         : 65001
+
+Date: 2017-11-19 18:26:45
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `sys_menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `icon` varchar(255) DEFAULT NULL,
+  `sort` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES ('5', '系统管理', null, null, '0', null);
+INSERT INTO `sys_menu` VALUES ('6', '菜单管理', '/sysMenu', null, '0', '5');
+INSERT INTO `sys_menu` VALUES ('23', '角色管理', '/sysRole', null, '2', '5');
+INSERT INTO `sys_menu` VALUES ('58', '组织机构', '/sysOrg', null, '3', '5');
+
+-- ----------------------------
+-- Table structure for `sys_oplog`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_oplog`;
+CREATE TABLE `sys_oplog` (
+  `id` varchar(32) NOT NULL,
+  `user_id` varchar(32) DEFAULT NULL,
+  `op_content` varchar(255) DEFAULT NULL,
+  `ip` varchar(100) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_oplog
+-- ----------------------------
+INSERT INTO `sys_oplog` VALUES ('918053748570324992', '916654989969981440', '访问无权限路径[/sysUser]', '0:0:0:0:0:0:0:1', '2017-10-11 18:00:44');
+INSERT INTO `sys_oplog` VALUES ('918059126326558720', '916654989969981440', '访问无权限路径[/sysUser]', '0:0:0:0:0:0:0:1', '2017-10-11 18:22:07');
+INSERT INTO `sys_oplog` VALUES ('919190626719760384', '916654989969981440', '访问无权限路径[/logout]', '0:0:0:0:0:0:0:1', '2017-10-14 21:18:17');
+INSERT INTO `sys_oplog` VALUES ('920277574486065152', '916654989969981440', '访问无权限路径[/sysMenu/query]', '0:0:0:0:0:0:0:1', '2017-10-17 21:17:26');
+INSERT INTO `sys_oplog` VALUES ('920277705956524032', '916654989969981440', '访问无权限路径[/sysMenu/query]', '0:0:0:0:0:0:0:1', '2017-10-17 21:17:57');
+
+-- ----------------------------
+-- Table structure for `sys_org`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_org`;
+CREATE TABLE `sys_org` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `org_name` varchar(50) DEFAULT NULL,
+  `sort` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_org
+-- ----------------------------
+INSERT INTO `sys_org` VALUES ('11', '金属公司', '0', null);
+INSERT INTO `sys_org` VALUES ('12', '物流公司', '1', null);
+INSERT INTO `sys_org` VALUES ('13', '房地产公司', '2', null);
+INSERT INTO `sys_org` VALUES ('14', '生产部', '0', '11');
+INSERT INTO `sys_org` VALUES ('15', '后勤部', '1', '11');
+INSERT INTO `sys_org` VALUES ('16', '货运部', '0', '12');
+INSERT INTO `sys_org` VALUES ('17', '销售部', '0', '13');
+INSERT INTO `sys_org` VALUES ('18', '营业部', '1', '12');
+
+-- ----------------------------
+-- Table structure for `sys_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) DEFAULT NULL COMMENT '角色名称',
+  `role_desc` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `sort` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('3', '管理员', '分配所有权限', '2017-10-07 21:22:34', '0');
+INSERT INTO `sys_role` VALUES ('12', '员工', 'staff', null, '2');
+INSERT INTO `sys_role` VALUES ('13', '主管', '主管', null, '3');
+INSERT INTO `sys_role` VALUES ('15', '1', '1', null, '4');
+
+-- ----------------------------
+-- Table structure for `sys_role_menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu` (
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  PRIMARY KEY (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role_menu
+-- ----------------------------
+INSERT INTO `sys_role_menu` VALUES ('3', '5');
+INSERT INTO `sys_role_menu` VALUES ('3', '6');
+INSERT INTO `sys_role_menu` VALUES ('3', '23');
+INSERT INTO `sys_role_menu` VALUES ('3', '58');
+
+-- ----------------------------
+-- Table structure for `sys_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` varchar(32) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(120) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `org_id` int(11) DEFAULT NULL COMMENT '部门id',
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(12) DEFAULT NULL,
+  `disabled` varchar(2) DEFAULT '0' COMMENT '是否禁用0 未禁用 1禁用',
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sys_user_org_id` (`org_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('916654989969981440', 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', '张闯', null, '916432779@qq.com', '15238002477', '0', '2017-10-07 21:22:34');
+
+-- ----------------------------
+-- Table structure for `sys_user_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
+  `user_id` varchar(32) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES ('916654989969981440', '3');
+
+-- ----------------------------
+-- Function structure for `getChildLst`
+-- ----------------------------
+DROP FUNCTION IF EXISTS `getChildLst`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `getChildLst`(rootId INT,tableName VARCHAR(255)) RETURNS varchar(1000) CHARSET utf8
+BEGIN
+   DECLARE sTemp VARCHAR(1000);
+   DECLARE sTempChd VARCHAR(1000);
+ 
+  SET sTemp = '$';
+   SET sTempChd =cast(rootId as CHAR);
+ 
+ 
+  WHILE sTempChd is not null DO
+    SET sTemp = concat(sTemp,',',sTempChd);
+    IF tableName = 'sys_org' THEN
+       SELECT group_concat(id) INTO sTempChd FROM sys_org where FIND_IN_SET(pid,sTempChd)>0;
+    ELSEIF tableName = 'sys_menu' THEN
+       SELECT group_concat(id) INTO sTempChd FROM sys_menu where FIND_IN_SET(pid,sTempChd)>0;
+    END IF;
+  END WHILE;
+  RETURN REPLACE(sTemp,'$,','');
+ END
+;;
+DELIMITER ;
